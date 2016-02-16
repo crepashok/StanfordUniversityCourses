58358fabbd16536d3a1b7092aaf6d0ac98f61bd3
Pavlo Cretsu <c.pavlo90@gmail.com>
2016-02-16 10:03:18 +0200

    Basic animation
    Main animation behaviours with rotation fix

---
+++ Dropit/Dropit/DropitBehavior.h
@@ -0,0 +1,17 @@
+//
+//  DropitBehavior.h
+//  Dropit
+//
+//  Created by Pavlo Cretsu on 2/15/16.
+//  Copyright © 2016 Pasha Cretsu. All rights reserved.
+//
+
+@import UIKit;
+
+@interface DropitBehavior : UIDynamicBehavior
+
+- (void)addItem:(id <UIDynamicItem>)item;
+
+- (void)removeItem:(id <UIDynamicItem>)item;
+
+@end
---
+++ Dropit/Dropit/ViewController.h
@@ -0,0 +1,16 @@
+//
+//  ViewController.h
+//  Dropit
+//
+//  Created by Pavlo Cretsu on 2/15/16.
+//  Copyright © 2016 Pasha Cretsu. All rights reserved.
+//
+
+@import UIKit;
+
+
+@interface ViewController : UIViewController
+
+
+@end
+
